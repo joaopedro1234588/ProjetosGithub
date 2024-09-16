775f624ded86274499a5d1420c6a6f839cfca33a
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-16 10:26:48 -0300

    Add files via upload

---
+++ 1A100/1A100/Program.cs
@@ -0,0 +1,6 @@
+﻿int soma = 0;
+for(int i = 1; i <= 100; i++)
+{
+    soma = soma + i;
+}
+Console.WriteLine(soma);
---
+++ de-1a-100/de-1a-100/Program.cs
@@ -0,0 +1,10 @@
+﻿int subitraçao = 0;
+
+for (int i = 1; i >= 100; i++)
+{
+    subitraçao = subitraçao - i;
+}
+
+Console.WriteLine(subitraçao);
+
+
