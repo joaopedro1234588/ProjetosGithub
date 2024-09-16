5ff49e52793b5ff89da80b051bb3ac85eda790f7
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-16 10:13:36 -0300

    Add files via upload

---
+++ somarNumerosAte0/fibonacciRecursivo/fibonacciRecursivo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de n para calcular o n-ésimo número da sequência de Fibonacci: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int resultado = Fibonacci(n);
+        Console.WriteLine($"O {n}-ésimo número da sequência de Fibonacci é: {resultado}");
+    }
+
+    static int Fibonacci(int n)
+    {
+        // Caso base
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        // Chamada recursiva
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
---
+++ somarNumerosAte0/somarNumerosAte0/Program.cs
@@ -0,0 +1,68 @@
+﻿int numero;
+int soma = 0;
+
+
+Console.WriteLine("digite um numero(0 para sair): ");
+numero = int.Parse(Console.ReadLine());
+soma += numero;
+while (numero! > 0) ;
+
+Console.WriteLine($"A soma total é: { soma}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
