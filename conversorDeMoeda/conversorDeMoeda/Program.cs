d7fe83f306c1f754533bc65ed54d10a76bf44d3a
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-16 10:16:45 -0300

    Add files via upload

---
+++ conversorDeMoeda/conversorDeMoeda/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Bem vindo ao conversor de moeda! Vamos converter para dólar");
+
+Console.WriteLine("Qual o valor que você quer converter ?");
+float valorParaConverter = float.Parse(Console.ReadLine());
+
+float converterMoeda(float valorParaConverter)
+{
+    float taxaDeConversao = float.Parse("5,57") ;
+    return (valorParaConverter * taxaDeConversao);
+}
+
+
+    Console.WriteLine(converterMoeda(valorParaConverter).ToString("f2"));
---
+++ joao/joao/Program.cs
@@ -0,0 +1,11 @@
+﻿string[]nomes = new string[4] { "joao", "maria", "jean","gabriel"};
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+
+
+
+
