6eaf9d71a4a1672a517666125c0c057369bdaf95
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-16 10:25:50 -0300

    Add files via upload

---
+++ calculadoraSImplis/calculadoraSImplis/calculadoraSImplis/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("Digite o primeiro número: ");
+double numero1 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite o segundo número: ");
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Escolha a operação (+, -, *, /): ");
+char operacao = Convert.ToChar(Console.ReadLine());
+
+double resultado;
+
+if (operacao == '+')
+{
+    resultado = numero1 + numero2;
+    Console.WriteLine($"O resultado da adição é: {resultado}");
+}
+else if (operacao == '-')
+{
+    resultado = numero1 - numero2;
+    Console.WriteLine($"O resultado da subtração é: {resultado}");
+}
+else if (operacao == '*')
+{
+    resultado = numero1 * numero2;
+    Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+}
+else if (operacao == '/')
+{
+    if (numero2 != 0)
+    {
+        resultado = numero1 / numero2;
+        Console.WriteLine($"O resultado da divisão é: {resultado}");
+    }
+    else
+    {
+        Console.WriteLine("Erro: Não é possível dividir por zero.");
+    }
+}
+else
+{
+    Console.WriteLine("Operação inválida!");
+}
+
+
---
+++ positivoEnegativo/positivoEnegativo/Program.cs
@@ -0,0 +1,10 @@
+﻿ Console.WriteLine("digite um numero para ver se ele e positivo ou negativo:");
+int numero = int.Parse(Console.ReadLine());
+
+
+    if (numero > 0)
+        Console.WriteLine("O número é positivo.");
+    else if (numero < 0)
+        Console.WriteLine("O número é negativo.");
+    else
+        Console.WriteLine("O número é zero.");
