68f4220a8afe2bb8a25d1f91e0159f628a069225
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-16 10:17:33 -0300

    Add files via upload

---
+++ idadeComForeach/idadeComForeach/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] idade = new string[6];
+
+for (int i = 0; i < 6; i++)
+{
+    if (i == 0)
+    {
+        Console.WriteLine("digite uma idade:");
+    }
+    else
+    {
+        Console.WriteLine("digite outra idade:");
+    }
+
+    idade[i] = Console.ReadLine();
+}
+
+foreach (string idades in idade)
+{
+    Console.WriteLine($"as idades sao essa {idades }");
+}
---
+++ quantidadeDeletrasDosNomes/quantidadeDeletrasDosNomes/Program.cs
@@ -0,0 +1,130 @@
+﻿/*List<string> nomes = new List<string> { "joao", "jean", "titi", "gabriel" };
+
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine($"Olá, {nome}!");
+}
+*/
+
+
+Carro cc = new Carro();
+cc.cor = "cinza";
+cc.marca = "honda";
+cc.acelerar();
+cc.modelo = "honda-fit";
+cc.valorFipe = 76738;
+Carro cg = new Carro();
+cg.cor = "laranja";
+cg.marca = "renault";
+cg.modelo = "kwid";
+cg.valorFipe += 44274;
+
+Console.WriteLine("esse e o carro do cesinha");
+Console.WriteLine(cc.marca +"  "+ cc.cor + "  "+cc.modelo + "  "+cc.valorFipe);
+Console.WriteLine("agora o carro do gabriel");
+Console.WriteLine(cg.marca + "  " + cg.cor + "  " + cg.modelo + "  " + cg.valorFipe);
+
+public class Carro
+{
+    public string cor { get; set; }
+    public string marca { get; set; }
+    public string modelo { get; set; }
+
+    public float valorFipe { get; set; }
+    public void acelerar()
+    {
+
+    }
+
+    public void freiar()
+    {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
