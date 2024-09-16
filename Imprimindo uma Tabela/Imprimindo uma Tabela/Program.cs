810a89b9a2b9b07c55b4e88a128f0e38a98296d7
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-16 10:23:47 -0300

    Add files via upload

---
+++ Imprimindo uma Tabela/Imprimindo uma Tabela/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Array com números inteiros
+        int[] numeros = { 2, 4, 6, 8, 10 };
+
+        // Variável para armazenar a soma
+        int soma = 0;
+
+        // Somando todos os elementos do array
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            soma += numeros[i];
+        }
+
+        // Exibindo a soma
+        Console.WriteLine($"A soma dos elementos do array é: {soma}");
+    }
+}
---
+++ abelaDeMultiplicação/abelaDeMultiplicação/Program.cs
@@ -0,0 +1,6 @@
+﻿for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= 10; j++) 
+        Console.Write($"{i * j}\n");
+    }
+    Console.WriteLine(); 
