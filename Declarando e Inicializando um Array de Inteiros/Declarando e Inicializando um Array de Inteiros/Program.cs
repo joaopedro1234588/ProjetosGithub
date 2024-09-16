c4a5155497ae3287666be315921d8dc59196dc23
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-16 10:22:45 -0300

    Add files via upload

---
+++ Declarando e Inicializando um Array de Inteiros/Declarando e Inicializando um Array de Inteiros/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numeros = new int[5];
+
+
+numeros[0] = 10;
+numeros[1] = 20;
+numeros[2] = 30;
+numeros[3] = 40;
+numeros[4] = 50;
+
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Elemento {i}: {numeros[i]}");
+}
---
+++ quadradoEstatico/quadradoEstatico/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+/*Console.WriteLine("insira o tamanho do quadrado:");
+int tamanho = int.Parse(Console.ReadLine());*/
+
+using System.Runtime.CompilerServices;
+using System.Xml;
+
+int tamanho = 4;
+
+
+for (int i = 1; i <= tamanho; i++)
+{
+    Console.WriteLine();
+
+    if (i == 1 || i == 4)
+    {
+        // executar o for que exibe ****
+        for (int j = 1; j <= tamanho; j++)
+        {
+            //Console.Write("* ");
+            Console.Write("* ");
+
+        }
+        //Console.WriteLine("* * * *");
+    }
+    else
+    {
+        
+        // executar o for que exibe *  *
+        for (int j = 1; j <= tamanho; j++)
+        {
+            //Console.Write("* ");
+            
+            if(j == 1 || j == 4)
+            {
+                Console.Write("* ");
+            }
+            else
+            {
+                Console.Write("  ");
+            }
+        }
+   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
