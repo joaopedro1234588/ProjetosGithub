0b59c1667decdbcc2aceb9d469f7a1ecf66e1b89
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-16 10:24:39 -0300

    Add files via upload

---
+++ nomerosParesComFor/nomerosParesComFor/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+Console.WriteLine("ola bem vindo ao jogo dos numeros pares: ");
+
+
+for  (int i = 2; i <= 20; i += 2)
+{
+    Console.WriteLine(i);
+}
---
+++ projeto gpt/projeto gpt/Program.cs
@@ -0,0 +1,41 @@
+﻿Random random = new Random();
+int numeroAleatorio = random.Next(1, 101);
+
+int palpite = 0;
+int tentativas = 0;
+
+Console.WriteLine("Bem-vindo ao jogo da adivinhação!\n");
+Console.WriteLine("Eu escolhi um número entre 1 e 100. Tente adivinhar!\n");
+
+
+while (palpite != numeroAleatorio)
+{
+    Console.Write("Digite seu palpite: ");
+    palpite = Convert.ToInt32(Console.ReadLine());
+    tentativas++;
+
+   
+    if (palpite < numeroAleatorio)
+    {
+        Console.WriteLine("O número é maior!");
+    }
+    else if (palpite > numeroAleatorio)
+    {
+        Console.WriteLine("O número é menor!");
+    }
+    else
+    {
+        Console.WriteLine($"Parabéns! Você adivinhou o número {numeroAleatorio} em {tentativas} tentativas.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
