1926bc582cf19e2c3ab8dfa3fc61d14595095791
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-17 09:24:28 -0300

    Add files via upload

---
+++ senhaCorreta/senhaCorreta/Program.cs
@@ -0,0 +1,14 @@
+﻿string senha = "1234@progamador";
+
+Console.WriteLine("digite a senha correta");
+string senha2 = Console.ReadLine();
+
+while (senha2 != senha)
+{
+    Console.WriteLine("senha invalida");
+
+    Console.WriteLine("digite novamente a senha ");
+    senha2 = Console.ReadLine();
+}
+
+Console.WriteLine("senha correta");
