6333bc858964bebc05076c3d0f75e529f2a7fb2f
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-17 10:49:36 -0300

    Add files via upload

---
+++ numeroSecreto/numeroSecreto/Program.cs
@@ -0,0 +1,34 @@
+﻿
+const int numeroSecreto = 7;
+
+int palpite = 0;
+
+
+Console.WriteLine("Tente adivinhar o número secreto!");
+
+while (palpite != numeroSecreto)
+{
+ 
+    Console.Write("Digite seu palpite: ");
+    
+    string input = Console.ReadLine();
+
+    
+    if (int.TryParse(input, out palpite))
+    {
+        if (palpite != numeroSecreto)
+        {
+           
+            Console.WriteLine("Palpite incorreto. Tente novamente.");
+        }
+    }
+    else
+    {
+        
+        Console.WriteLine("Por favor, insira um número válido.");
+    }
+}
+
+
+Console.WriteLine("Parabéns! Você acertou o número secreto!");
+
