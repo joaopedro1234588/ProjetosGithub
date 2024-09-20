f0818415c0f4d983790194ca0f2fced813d66369
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-09-20 11:55:54 -0300

    Add files via upload

---
+++ sistemaDeReservasAereas/sistemaDeReservasAereas/Program.cs
@@ -0,0 +1,91 @@
+﻿using sistemaDeReservasAereas;
+using System;
+
+Console.WriteLine("-------------------------------------------------------------------------");
+Console.WriteLine("|                          PIX AIRLINES                                 |");
+Console.WriteLine("|                                                                       |");
+Console.WriteLine("|                    1.CRIAR RESERVA                                    |");
+Console.WriteLine("|                    2.EDITAR RESERVA                                   |");
+Console.WriteLine("|                    3.CANCELAR RESERVA                                 |");
+Console.WriteLine("|                    4.CONSULTAR RESERVA                                |");
+Console.WriteLine("|                                                                       |");
+Console.WriteLine("-------------------------------------------------------------------------");
+
+Console.WriteLine("escolha uma opçao acima!");
+int opcaoEscolhida = int.Parse(Console.ReadLine());
+
+
+
+switch (opcaoEscolhida)
+{
+    case 1:
+        Console.Clear();
+        criarReserva();
+        break;
+    case 2:
+        editarReserva();
+        break;
+    case 3:
+        cancelarReserva();
+        break;
+    case 4:
+        consultarReserva();
+        break;
+    default:
+        Console.WriteLine("Opção inválida");
+        break;
+}
+
+
+
+void criarReserva()
+{
+    Console.WriteLine("*Vamos criar uma reserva:"); 
+    Console.WriteLine("Seu nome para a reserva:*");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("Numero do voo:");
+    int numeroDoVoo = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Numero da reserva:");
+    int numeroDeReserva = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Numero de telefone de algum familiar:");
+    int telefone = int.Parse(Console.ReadLine());
+
+    //Reserva reservapix = new Reserva("joao","747","2578","1469654799");
+
+
+
+
+
+
+
+
+
+}
+
+void editarReserva()
+{
+
+}
+
+
+void cancelarReserva()
+{
+
+}
+
+
+void consultarReserva()
+{
+
+}
+
+
+
+
+
+
+
+
---
+++ sistemaDeReservasAereas/sistemaDeReservasAereas/Reserva.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace sistemaDeReservasAereas
+{
+    public class Reserva
+    {
+        public string Nome;
+        public string NumeroDoVoo;
+        public string NumeroDeReservas;
+        public string Telefone;
+
+        public Reserva(string nome, string numeroDoVoo, string numeroDeReservas, string telefone)
+        {
+                
+            Nome = nome;
+            NumeroDeReservas = numeroDeReservas;
+            Telefone = telefone;
+            NumeroDoVoo = numeroDoVoo;
+        
+            
+        
+        
+        
+        
+        }
+
+    }
+}
