2617e5cd66ce285f0bd98e55ac9c318005cdad3d
joaopedro1234588 <joaopedrofernandes26102010@gmail.com>
2024-10-07 09:54:51 -0300

    Add files via upload

---
+++ PixSaound/Album.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+class Album
+{
+
+    private List<Musica> musicas = new List<Musica>();
+
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get;}
+
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de musica do album{Nome}:\n");
+        foreach (var musica in musicas)
+        {
+        Console.WriteLine($"Musica : {musica.Nome}");
+
+        }
+        Console.WriteLine($"\n para ouvir este album inteiro vc precisa de {DuracaoTotal}");
+    }
+}
